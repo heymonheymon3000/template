451a4aae333321978ae572989b8ebde00ecc15ed
Terry Parrish <terry.1.parrish@gm.com>
2022-11-06 06:34:21 -0500

    adding

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_gm_template_application_BaseApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.gm.template.application.BaseApplication"
-)
-public final class _com_gm_template_application_BaseApplication {
-}
